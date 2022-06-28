28e2c1e05d035f37bd5c7b9706e67b4f69e70f5d
= <=>
2022-06-28 10:15:56 +0400

    Langind page ready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
---
+++ src/routes/navigation/navigation.component.js
@@ -0,0 +1,25 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import { ReactComponent as CrwnLogo } from "../../assets/logo/crown.svg";
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <div>
+          <Link className="nav-logo-container" to="/">
+            <CrwnLogo />
+          </Link>
+        </div>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+export default Navigation;
