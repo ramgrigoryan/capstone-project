d767779680eada038c3f3397dbe90de9c093ae51
Vahram <ramgrigoryan@gmail.com>
2022-07-05 12:13:25 +0400

    Shop page rendered

--- src/routes/shop/shop.component.js
+++ src/routes/shop/shop.component.js
@@ -1,14 +1,17 @@
 import { useContext } from "react";
 import { ProductsContext } from "../../contexts/products.context";
+import Product from "../../components/product/product.component";
+import "./shop.styles.scss"
 
 const Shop = () => {
   const { products } = useContext(ProductsContext);
   return (
-    <div>
-      {products.map(({ id, name }) => {
+    <div className="products-container">
+      {products.map((product) => {
+        console.log(product)
         return (
-          <div key={id}>
-            <h1>{name}</h1>
+          <div key={product.id}>
+            <Product product={product} />
           </div>
         );
       })}
